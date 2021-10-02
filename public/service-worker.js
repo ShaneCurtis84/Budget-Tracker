18eff4a19b59f48a4efc42bb28a612343b9667a9
Shane Curtis <c84a@ymail.com>
2021-10-02 20:16:44 +0800

    Deployed to heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -48,7 +48,7 @@ self.addEventListener("fetch", function (evt) {
 
   if (evt.request.method !== "GET") {
     return;
-}
+  }
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
       caches
@@ -77,15 +77,14 @@ self.addEventListener("fetch", function (evt) {
   // if the request is not for the API, serve static assets using "offline-first" approach.
   // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
   evt.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-    return cache.match(evt.request).then(response => {
-      return response || fetch(evt.request);
-    });
-      
-    })
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.match(evt.request).then((response) => {
+          return response || fetch(evt.request);
+        });
+      })
 
-    .catch(err => console.log(err))
-
+      .catch((err) => console.log(err))
   );
 });
-
